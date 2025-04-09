52ed5ee870e3524e59e484901c74e26c1f3c7e43
0xcoca <maynardiona1@gmail.com>
2025-04-09 19:58:22 +0800

    test: deploy

--- components/Rescue/Form/index.tsx
+++ components/Rescue/Form/index.tsx
@@ -6,7 +6,7 @@ const RescueForm: React.FC<Props> = () => {
   return (
     <div>
       <h1>Rescue Form</h1>
-      <p>This is the rescue form component.</p>
+      <p>This is the rescue form component..</p>
     </div>
   )
 } 
